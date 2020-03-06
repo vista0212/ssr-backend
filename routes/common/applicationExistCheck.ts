1fbf0b9600b31618cd0cd0c7dea2c5c8e4683dcc
kb4ek <kbaek1202@gmail.com>
2020-03-06 13:25:05 +0900

    fixed: change endpoint

--- routes/common/applicationExistCheck.ts
+++ routes/common/applicationExistCheck.ts
@@ -6,20 +6,22 @@ import { throwError, catchDBError } from '@Lib/error';
 const applicationExistCheck = async (req: Request, res: Response, next: NextFunction) => {
   const email: Application['email'] = req.body.email;
 
+  console.log(req.url);
+
   const application: Application = await Application.findOne({
     where: {
       email
     }
   }).catch(catchDBError(res));
 
-  if (req.url == '/') {
+  if (req.url == '/application') {
     if (application) {
       throwError(res, 'Exist_Data', '중복된 신청서입니다.');
     }
     next();
   }
 
-  if (req.url == '/load') {
+  if (req.url == '/application/load') {
     if (!application) {
       throwError(res, 'Not_Found', '존재하지 않는 신청서입니다.');
     }
